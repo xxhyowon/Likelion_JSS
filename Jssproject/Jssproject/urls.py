352591d14feb5a089fd5de3a5754e65fd1c2c908
xxhyowon <iios31@likelion.org>
2020-08-17 03:17:12 +0900

    html

--- Jssproject/Jssproject/urls.py
+++ Jssproject/Jssproject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Jssapp_main.views import index, create
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index, name = 'index'),
+    path('create/', create, name = 'create'),
 ]
