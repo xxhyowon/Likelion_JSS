cc6a9bfec7ce1a594a6f564d1a6f022329e248b5
xxhyowon <iios31@likelion.org>
2020-08-18 15:43:18 +0900

    modelform

--- Jssproject/Jssapp_main/views.py
+++ Jssproject/Jssapp_main/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import JssForm
+from .models import Jasoseol
 # Create your views here.
 
 def index(request) :
-    
-    return render(request, 'index.html')
+    all_jss = Jasoseol.objects.all()
+    return render(request, 'index.html', {'all_jss' : all_jss})
 
 def create(request) :
-
-    return render(request, 'create.html')
+    if request.method == 'POST' :
+        filled_form = JssForm(request.POST)
+        if filled_form.is_valid():
+            filled_form.save()
+            return redirect('index')
+    jss_form = JssForm()
+    return render(request, 'create.html', {'jss_form' : jss_form})
