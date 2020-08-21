80d5529097edba8db98f3d9df6c04f05333a4134
xxhyowon <iios31@likelion.org>
2020-08-21 22:06:26 +0900

    log/templates

--- Jssproject/Jssapp_accounts/views.py
+++ Jssproject/Jssapp_accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.views import LoginView
 # Create your views here.
 
 def signup(request) :
@@ -10,3 +11,7 @@ def signup(request) :
             filled_form.save()
             return redirect('index')
     return render(request, 'signup.html', {'regi_form': regi_form})
+
+class MyLoginView(LoginView) :
+    template_name = 'login.html'
+
